448fbb61d6f8d54a4082e85db1a5af8a3d32b6b5
Gilberto Sá <gibasa@gmail.com>
2023-06-30 21:34:02 -0300

    createNotes function and component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import './App.css'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
+import notes from "./assets/notes"
+
+function createNotes(note){
+    return <Note
+    key={note.id}
+    title={note.title}
+    content={note.content}
+    />
+}
 
 function App() {
   
   return (
     <>
       <Header/>
-      <Note/>
+      {notes.map(createNotes)}
       <Footer/>
     </>
   )
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,12 @@
-function Note(){
+import PropTypes from "prop-types"
+function Note(props){
     return(
         <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 }
 
 export default Note
+Note.propTypes = { title: PropTypes.string, content: PropTypes.string };
